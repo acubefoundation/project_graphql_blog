9b623dc7bcbbd5d13fb786fd690f50c20a6f3ef8
acubefoundation <131584914+acubefoundation@users.noreply.github.com>
2023-08-15 09:32:07 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ module.exports = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
-},
+  },
+  future: {
+    webpack5: true, // Enable Webpack 5 for improved performance and features
+  },
 };
